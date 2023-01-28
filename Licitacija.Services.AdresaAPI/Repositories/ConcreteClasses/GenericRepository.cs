fe1bd2a77d9c64ab76cbe64ea1efd81a8caa319b
Maja Jukic <mjukic2000@gmail.com>
2023-01-28 16:55:39 +0100

    Ispravke u GenericRepository klasi

--- Licitacija.Services.AdresaAPI/Repositories/ConcreteClasses/GenericRepository.cs
+++ Licitacija.Services.AdresaAPI/Repositories/ConcreteClasses/GenericRepository.cs
@@ -22,18 +22,6 @@ namespace Licitacija.Services.AdresaAPI.Repositories.ConcreteClasses
             _db.Remove(entity);
         }
 
-        public async Task<T> Get(Expression<Func<T, bool>>? expression = null)
-        {
-            IQueryable<T> query = _db;//uzima sve torke koji su u tabeli datog tipa T
-
-            return await query.AsNoTracking().FirstOrDefaultAsync(expression);//expression se odnosi na ono po cemu se filtriraju podaci npr po id(lambda)
-        }
-
-        public async Task<IList<T>> GetAll()
-        {
-            return await _db.AsNoTracking().ToListAsync();//asnotracking ne sacuvavavju se izmene automatski (brze i koristi se za get req)
-        }
-
         public async Task Insert(T entity)
         {
             await _db.AddAsync(entity);
@@ -41,7 +29,7 @@ namespace Licitacija.Services.AdresaAPI.Repositories.ConcreteClasses
 
         public void Update(T entity)
         {
-            _db.Attach(entity);//provera da li postoje neke razlike izmedju ovog entiteta i onoga sta postoji u bazi
+            _db.Attach(entity);//provera da li postoje neke razlike izmedju ovog entiteta i onoga sta postoji u bazi i primenjuje ih
             _context.Entry(entity).State = EntityState.Modified;
         }
     }
