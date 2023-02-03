52bbd4f57e9f4647347dba64a1a772da5a14f357
Maja Jukic <mjukic2000@gmail.com>
2023-02-03 12:52:48 +0100

    Connection to the Database and DbContext

---
+++ Licitacija.Services.PredradnjeNadmetanjaAPI/Entities/PredradnjeNadmetanja.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Licitacija.Services.PredradnjeNadmetanjaAPI.Entities
+{
+    [Index(nameof(PredradnjeNadmetanjaNaziv), IsUnique = true)]
+    public class PredradnjeNadmetanja
+    {
+        [Key]
+        public Guid PredradnjeNadmetanjaId { get; set; }
+
+        public string PredradnjeNadmetanjaNaziv { get; set; }  = String.Empty;
+    }
+}
--- Licitacija.Services.PredradnjeNadmetanjaAPI/Program.cs
+++ Licitacija.Services.PredradnjeNadmetanjaAPI/Program.cs
@@ -1,6 +1,14 @@
+using Licitacija.Services.PredradnjeNadmetanjaAPI.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//DbContext
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration
+            .GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
