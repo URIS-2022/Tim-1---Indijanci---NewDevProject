2935b1dba02dc67cb1ef804eab6546cd9b711bb0
Maja Jukić <73577538+majajukic@users.noreply.github.com>
2023-02-07 14:35:32 +0100

    Feature/kupac (#8)
    * Databse connection and DbContext setup
    
    * Implementacija CRUD operacija za entitete Prioritet i KontaktOsoba
    
    * Dokumentovanje Prioriteta i KontaktOsoba
    
    * Implementacija CRUD operacija za entitete Kupac, FL i PL
    
    * Ispravke vezane za brisanje FL i PL i dokumentovanje
    
    * Dodatne ispravke
    
    * Ispravke
    
    * Dodavanje String.Empty na obelezja u entity klasama
    
    * izmena gitignore fajla
    
    * appsettings.json fajl
    
    * Izmena u dokumentovanju
    
    * Integracija Kupca i Adrese sa izmenama
    
    * Novi endponint za Kupac

---
+++ Licitacija.Services.KupacAPI/Repositories/ConcreteClasses/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Licitacija.Services.KupacAPI.DbContexts;
+using Licitacija.Services.KupacAPI.Entities;
+using Licitacija.Services.KupacAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Licitacija.Services.KupacAPI.Repositories.ConcreteClasses
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DataContext _context;
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(DataContext context)
+        {
+            _context = context;
+            _db = _context.Set<T>();
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> expression)
+        {
+            return await _db.AsNoTracking().FirstOrDefaultAsync(expression);
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _db.AsNoTracking().ToListAsync();
+        }
+
+        public async Task Delete(Guid guid)
+        {
+            var entity = await _db.FindAsync(guid);
+            _db.Remove(entity);
+        }
+
+        public async Task Insert(T entity)
+        {
+            await _db.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _db.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Licitacija.Services.KupacAPI/Repositories/Interfaces/IKupacRepository.cs
@@ -0,0 +1,13 @@
+﻿using Licitacija.Services.KupacAPI.Entities;
+
+namespace Licitacija.Services.KupacAPI.Repositories.Interfaces
+{
+    public interface IKupacRepository
+    {
+        Task<IEnumerable<Kupac>> GetAll();
+
+        Task<Kupac> Get(Guid id);
+
+        Task<Kupac> GetKupacBasicInfo(Guid kupacId);
+    }
+}
