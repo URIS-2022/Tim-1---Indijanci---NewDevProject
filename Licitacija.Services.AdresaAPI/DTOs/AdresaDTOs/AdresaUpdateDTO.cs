23cd2e7a271b848158fa0bff7253841e8f7a6b59
Maja Jukic <mjukic2000@gmail.com>
2023-02-11 15:00:54 +0100

    ispravke

--- Licitacija.Services.AdresaAPI/DTOs/AdresaDTOs/AdresaUpdateDTO.cs
+++ Licitacija.Services.AdresaAPI/DTOs/AdresaDTOs/AdresaUpdateDTO.cs
@@ -50,17 +50,17 @@ namespace Licitacija.Services.AdresaAPI.DTOs.Adresa
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            var regexItem = new Regex("^[a-zA-Z0-9 ]*$");
+            var regexItem = new Regex("^[a-zA-Z0-9 ]*$", RegexOptions.None, TimeSpan.FromMilliseconds(100));
 
             if (!regexItem.IsMatch(Broj) || !regexItem.IsMatch(PostanskiBroj)) {
-
-                AppDomain.CurrentDomain.SetData("REGEX_DEFAULT_MATCH_TIMEOUT", TimeSpan.FromMilliseconds(100));
 
                 yield return new ValidationResult(
                   "Nije moguće izmeniti adresu zato sto neki unosi sadrže specijalne karaktere.",
                   new[] { "AdresaUpdateDTO" });
 
             }
+
+            AppDomain.CurrentDomain.SetData("REGEX_DEFAULT_MATCH_TIMEOUT", TimeSpan.FromMilliseconds(100));
 
         }
     }
