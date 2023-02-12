662cd6470e4446ed4cafae0c10de67bd7d0b19b8
Maja Jukić <73577538+majajukic@users.noreply.github.com>
2023-02-12 19:10:30 +0100

    Feature/predradnje nadmetanja (#25)
    * Prvi commit
    
    * Connection to the Database and DbContext
    
    * Implementacija mikroservisa PredradnjeNadmetanja
    
    * dodavanje gitignore fajla
    
    * uklanjanje user fajla
    
    * Dokumentovanje predradnje nadmetanja
    
    * Update grane
    
    * Novi endpoint i brisanje nepotrebnih provera
    
    * Izmena endpointa basic info
    
    * Ispravke - tehnicki dug
    
    * Mock service za ms Faza
    
    * Ispravke 1 - tehnicki dug
    
    * Integracija Predradnji sa FazomLicitacije

--- Licitacija.Services.PredradnjeNadmetanjaAPI/DTOs/FazaDto.cs
+++ Licitacija.Services.PredradnjeNadmetanjaAPI/DTOs/FazaDto.cs
@@ -1,9 +1,19 @@
 ﻿namespace Licitacija.Services.PredradnjeNadmetanjaAPI.DTOs
 {
+    /// <summary>
+    /// DTO faze licitacije.
+    /// </summary>
     public class FazaDto
     {
+        /// <summary>
+        /// ID faze licitacije.
+        /// </summary>
         public Guid FazaId { get; set; }
 
-        public string FazaNaziv { get; set; }
+        /// <summary>
+        /// Naziv faze Licitacije
+        /// </summary>
+        public String FazaNaziv { get; set; }
+
     }
 }
