211fcd112b0883179283156b898f22225facce43
MasaSaranovic <73577506+MasaSaranovic@users.noreply.github.com>
2023-02-13 09:14:54 +0100

    Feature/zalba (#28)
    * Odradjen mikroservis Zalba
    
    * Dorada integracije sa ms Kupac

---
+++ Licitacija.Services.ZalbaAPI/Entities/RadnjaNaOsnovuZalbe.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Licitacija.Services.ZalbaAPI.Entities
+{
+    /// <summary>
+    /// Predstavlja radnju za žalbu
+    /// </summary>
+    [Index(nameof(RadnjaNaziv), IsUnique = true)]
+    public class RadnjaNaOsnovuZalbe
+    {
+        /// <summary>
+        /// ID radnje za žalbu
+        /// </summary>
+        [Key]
+        public Guid RadnjaId { get; set; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Naziv radnje za žalbu
+        /// </summary>
+        [Required]
+        public string RadnjaNaziv { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Lista kupaca sa datim prioritetom.
+        /// </summary>
+        [JsonIgnore]
+        public IList<Zalba>? Zalba { get; set; }
+    }
+}
