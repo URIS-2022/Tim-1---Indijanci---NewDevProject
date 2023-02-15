49300465e4664167a043f9482a014b1bc18ca2e0
kristinatadic00 <73581344+kristinatadic00@users.noreply.github.com>
2023-02-15 17:22:22 +0100

    Zavrsne Radnje Mikroservis (#35)

---
+++ Licitacija.Services.ZavrsneRadnjeAPI/Profiles/ZavrsneRadnjeProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Licitacija.Services.ZavrsneRadnjeAPI.DTOs.ZavrsneRadnjeDto;
+using Licitacija.Services.ZavrsneRadnjeAPI.Entities;
+
+namespace Licitacija.Services.ZavrsneRadnjeAPI.Profiles
+{
+    public class ZavrsneRadnjeProfile : Profile
+    {
+        public ZavrsneRadnjeProfile()
+        {
+            CreateMap<ZavrsneRadnje, ZavrsneRadnje>();
+            CreateMap<ZavrsneRadnje, ZavrsneRadnjeDto>().ReverseMap();
+            CreateMap<ZavrsneRadnje, ZavrsneRadnjeUpdateDto>().ReverseMap();
+            CreateMap<ZavrsneRadnje, ZavrsneRadnjeCreateDto>().ReverseMap();
+        }
+    }
+}
