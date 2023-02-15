a79bb44426aafcdb5c7f8aee188ae7ff4c7c3ed0
Selena Delčev <73577585+SelenaDelcev@users.noreply.github.com>
2023-02-15 17:25:42 +0100

    Etapa Mikroservis (#36)
    Co-authored-by: SelenaDelcev <selenadelcev411@gmail.com>

---
+++ Licitacija.Services.EtapaAPI/DbContexts/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Licitacija.Services.EtapaAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Licitacija.Services.EtapaAPI.DbContexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Etapa> Etape { get; set; }
+    }
+}
