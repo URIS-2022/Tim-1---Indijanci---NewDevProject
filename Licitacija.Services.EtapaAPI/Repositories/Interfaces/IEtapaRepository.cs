801718e5d9bffa68eec4552fb23bdbc95a5dd9eb
SelenaDelcev <selenadelcev411@gmail.com>
2023-02-15 17:24:25 +0100

    Etapa Mikroservis

---
+++ Licitacija.Services.EtapaAPI/Repositories/Interfaces/IEtapaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Licitacija.Services.EtapaAPI.Entities;
+
+namespace Licitacija.Services.EtapaAPI.Repositories.Interfaces
+{
+    public interface IEtapaRepository
+    {
+        List<Etapa> GetAll();
+        Etapa GetEtapa (Guid id);
+        void InsertEtapa(Etapa etapa);
+        void DeleteEtapa(Guid id);
+        void UpdateEtapa(Etapa etapa);
+        bool Save();
+    }
+}
