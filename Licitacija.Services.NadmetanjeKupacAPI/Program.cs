59ace3ec4c1a4c37da1ca617b9c7af61a33f630d
MasaSaranovic <masasaranovic@gmail.com>
2023-02-17 14:03:10 +0100

    Izmena ms NadmetanjeKupac

--- Licitacija.Services.NadmetanjeKupacAPI/Program.cs
+++ Licitacija.Services.NadmetanjeKupacAPI/Program.cs
@@ -7,16 +7,17 @@ using System.Reflection;
 using Licitacija.Services.NadmetanjeKupacAPI.Repositories.Interfaces;
 using Licitacija.Services.NadmetanjeKupacAPI.Repositories.ConcreteClasses;
 using Licitacija.Services.NadmetanjeKupacAPI.ServiceCalls;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(setup =>
-{
-    setup.ReturnHttpNotAcceptable = true;
-}
-).AddXmlDataContractSerializerFormatters()
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+
+//setup.ReturnHttpNotAcceptable = true;
+options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+    .AddXmlDataContractSerializerFormatters()
             .ConfigureApiBehaviorOptions(setupAction =>
             {
                 setupAction.InvalidModelStateResponseFactory = context =>
@@ -100,7 +101,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<DataContext>();
-    context.Database.Migrate();
+    //context.Database.Migrate();
 }
 
 // Configure the HTTP request pipeline.
