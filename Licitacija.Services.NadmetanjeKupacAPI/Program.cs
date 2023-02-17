2c3e24dafc747bd6adddf8b015e76706781c7251
MasaSaranovic <masasaranovic@gmail.com>
2023-02-17 15:38:14 +0100

    Integracija ms NadmetanjeKupac sa ms Nadmetanje

--- Licitacija.Services.NadmetanjeKupacAPI/Program.cs
+++ Licitacija.Services.NadmetanjeKupacAPI/Program.cs
@@ -101,7 +101,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<DataContext>();
-    //context.Database.Migrate();
+    context.Database.Migrate();
 }
 
 // Configure the HTTP request pipeline.
