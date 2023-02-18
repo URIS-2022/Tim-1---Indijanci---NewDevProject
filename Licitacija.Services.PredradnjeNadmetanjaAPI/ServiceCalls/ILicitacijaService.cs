3dc6e0b6eee30a6c2965dcb24a9928f08d4c91ff
FilipPiptorosevic <73577335+FilipPiptorosevic@users.noreply.github.com>
2023-02-18 14:02:03 +0100

    Revert "Feature/zalba (#27)"
    This reverts commit 19f0c23704a923e9685a2cc86571eefb7d569308.

--- Licitacija.Services.PredradnjeNadmetanjaAPI/ServiceCalls/ILicitacijaService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Licitacija.Services.PredradnjeNadmetanjaAPI.DTOs;
-
-namespace Licitacija.Services.PredradnjeNadmetanjaAPI.ServiceCalls
-{
-    public interface ILicitacijaService
-    {
-        public Task<FazaDto> GetFazaById(Guid fazaid);
-    }
-}
